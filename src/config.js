fc1dd122cfa20e57067b328a97529db2430791b9
gonzalobenini <95836577+gonzalobenini@users.noreply.github.com>
2023-01-17 19:58:25 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWROD = process.env.DB_PASSWROD || '4 p41r of kn13v3s';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '4 p41r of kn13v3s';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || '3306';
 export const DB_DATABASE = process.env.DB_DATABASE || 'tarjetas';
